2f853c5bbfcf1de13ceaf732df9ab2e2fcc6119c
S Mautner <mautner@cs.uni-freiburg.de>
2018-02-01 12:09:40 +0100

    gradbosting

--- optimize_gradient_boosting.py
+++ optimize_gradient_boosting.py
@@ -1,8 +1,10 @@
 from scipy.stats import randint as rint
 from scipy.stats import uniform as uni
-from sklearn.ensemble import GradientBoostingRegressor
+
+#from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import RandomizedSearchCV as rsearch
 import simushape as ss
+import xgboost
 
 
 def quickladdata():
@@ -19,25 +21,26 @@ def uniform(lower, upper):
     return uni(lower, upper-lower)
 
 def opti_forest(data,r=3,d=3, n_jobs=1,n_iter=10):
-    model = GradientBoostingRegressor()
+    model = xgboost.XGBRegressor()
     param_dist = {
-                     'loss': ['ls', 'lad', 'huber', 'quantile'],
+                     'max_depth': rint(1, 7),
                      'learning_rate': uniform(0.01,0.2),
-                     'max_depth': rint(1, 6),
-                     'min_samples_split': rint(2, 5),
-                     'min_samples_leaf': rint(1, 3),
-                     'min_weight_fraction_leaf': uniform(0,.05),
-                     'max_features': ["auto", 'sqrt' , 'log2'],
-                     'alpha': uniform(0.8,0.97)
+                        'n_estimators': rint(50, 200),
+                          'booster': ['gbtree', 'gblinear' , 'dart'],
+                    'gamma':[0,0.005,0.001],
+        'min_child_weight' : [1,2,3],
+'max_delta_step' : [0,1,2],
+'reg_alpha' : uniform(.5,1),
+'reg_lambda' : uniform(0.5,1)
     }
 
     X,y = ss.getXY(data,data.keys(),r,d)
-    X=X.toarray()
+    #X = xgboost.DMatrix(X)
+    #y= xgboost.DMatrix(y)
     blu = rsearch(model, param_distributions=param_dist, n_iter=n_iter,n_jobs=n_jobs)
     blu.fit(X, y)
-
     print blu.best_params_
     print blu.best_score_
 
 
-opti_forest(data, n_jobs=4, n_iter=8)
+opti_forest(data, n_jobs=1, n_iter=10)
