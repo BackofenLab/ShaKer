e6a842526808b9523f31e13aa78d850e23144781
S Mautner <mautner@cs.uni-freiburg.de>
2018-07-10 23:35:15 +0200

    rnasubopt cache improved

--- rna_tools/rnasubopt.py
+++ rna_tools/rnasubopt.py
@@ -1,4 +1,5 @@
 from rna_tools import shexec
+import os
 import re
 
 
@@ -9,16 +10,19 @@ def rnasubopt(sequence, return_energy=False):
 
         d={}
         if len(sequence) > 300:
-            d = rna_tools.loadfile(".rnasubopt_cache")
-            if sequence in d:
-                return d[sequence] if return_energy else d[sequence][0]
+            if os.path.isfile(".rnasubopt_cache"):
+                d = rna_tools.loadfile(".rnasubopt_cache")
+                if sequence in d:
+                    return d[sequence] if return_energy else d[sequence][0]
+            else:
+                rna_tools.dumpfile({}, '.rnasubopt_cache')
         retcode, err, out = shexec("echo " + "%s" % sequence + " | RNAsubopt -p 60 --maxBPspan=150")
         energy = re.findall(r"[-+]?[0-9]*\.?[0-9]+", out)
         shape = [a.strip() for a in re.findall(r'\n[.()]+', out)]
 
         if len(sequence) > 300:
             d[sequence]= (shape, energy)
-            rnasubopt.dumpfile(d,".rnasubopt_cache")
+            rna_tools.dumpfile(d,".rnasubopt_cache")
 
         if return_energy:
             return shape, energy
