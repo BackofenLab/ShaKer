0e2879d8aeaef9fa520b4e65798c72927b72c990
Soheila Montaseri <montaser@bipc42.informatik.uni-freiburg.de>
2018-07-26 15:11:05 +0200

    extract_genes.py was cleaned

--- extract_genes.py
+++ extract_genes.py
@@ -3,46 +3,68 @@ path="/home/montaser/ShaKer/data/weeks194_orig/"
         
 #for genes
 def extractgenes():
-    endIdxG = 0
+    endIdxG = 0 
     i = 0
-    dbnfileGenes = open(path + "cellfreeGenes.dbn", "w") # we write s.th
-    with open(path + "genes.txt", "r") as g:  # we read something
+    filew = openfile("cellfreeGenes.dbn", "w")
+    filer = openfile("genes.txt", "r")
+    extractfilerInfo(filer, filew, endIdxG, i)# extract information from read file
+                
+                
+def openfile(filename, type):
+    if type == "w":
+        file = open(path + filename, "w") # write into a file
+    else: file = open(path + filename, "r") # read a file
+    return file 
+
+def extractfilerInfo(filer, filew, endIdxG, i, reactdict = None):
+    with filer as g:  # read readable file
         g.next()
         for e in g:
             if "," in e:  # all the lines with "," are interesting
                 name, Gstart, Gend, Tstart, Tend, idx =  e.strip().split(',')[0:6]
-                assert (int(Gend) - int(Gstart)) == (int(Tend) - int(Tstart)) 
-                endIdxG, i = extractTranscriptomes(idx, Tstart, Tend, dbnfileGenes, endIdxG, i)
+                assert (int(Gend) - int(Gstart)) == (int(Tend) - int(Tstart))
+                l = extractLSeq(idx)
+                if reactdict == None: endIdxG, i = extractTranscriptomes(idx, Tstart, Tend, filew, endIdxG, i)
+                else: endIdxG, i, reactdict = computereact(idx, Tstart, Tend, l, filew, endIdxG, i, reactdict)
 
-def extractTranscriptomes(idx, Tstart, Tend, dbnfileGenes, endIdxG, i):
-    with open(path + "cellfree.dbn", "r") as D:
+def extractTranscriptomes(idx, Tstart, Tend, filew, endIdxG, i):
+    cutoff = 20
+    structure = ""
+    filer = openfile("cellfree.dbn", "r")
+    with filer as D:
         for d in D:
-            if ">" in d:
-                namesidx = d.strip().split('_')
-                indx = namesidx[len(namesidx)-1]
-                if indx == idx:
+            if ">" in d: 
+                namesidx = d.strip().split('_')# CONTAINS NAME AND INDEX
+                if namesidx[len(namesidx)-1] == idx:
                     i = i + 1
                     seq = D.next()
-                    if endIdxG == 0:
-                        startIdxT = endIdxG
-                    else: startIdxT = endIdxG + 1
-                    startIdxG = int(Tstart)
+                    if endIdxG == 0: # write the first transcript
+                        startIdxT = endIdxG 
+                        if len(seq[startIdxT: int(Tstart) - 1]) >= cutoff: 
+                            structure = makeStructure(len(seq[startIdxT: int(Tstart) - 1]))
+                            filew.write(">%s\n%s\n%s\n" % ("T_" + idx + "_" + str(i), seq[startIdxT: int(Tstart) - 1], structure))
+                    else: # write the rest transcript
+                        startIdxT = endIdxG + 1 
+                        if len(seq[startIdxT - 1: int(Tstart) - 1])>= cutoff: 
+                            structure = makeStructure(len(seq[startIdxT - 1: int(Tstart) - 1]))
+                            filew.write(">%s\n%s\n%s\n" % ("T_" + idx + "_" + str(i), seq[startIdxT - 1: int(Tstart) - 1], structure))
                     endIdxG = int(Tend)
-                    endIdxT = startIdxG
-                    nameT = "T_" + idx + "_" + str(i)
-                    if startIdxT == 0:
-                        text =">%s\n%s\n" % (nameT, seq[startIdxT: endIdxT - 1])
-                    else: text =">%s\n%s\n" % (nameT, seq[startIdxT - 1: endIdxT - 1])
-                    dbnfileGenes.write(text)
-                    if i == len(namesidx) - 1: 
-                        text = ">%s\n%s\n" % ("T_" + idx + "_" + str(i+1), seq[endIdxG: ])
-                        dbnfileGenes.write(text)
+                    if i == len(namesidx) - 1: # write the last transcript
+                        if len(seq[endIdxG: ]) - 1 >= cutoff: 
+                            structure = makeStructure(len(seq[endIdxG: ]) -1)
+                            filew.write(">%s\n%s%s\n" % ("T_" + idx + "_" + str(i+1), seq[endIdxG: ], structure))
                         endIdxG = 0
                         i = 0
                     break
-
     return (endIdxG, i)
-
+                    
+def makeStructure(lseq):
+    struct = ""
+    for k in range(lseq):
+        struct = struct + "."
+    return struct
+   
+    
 #for reactivities                    
 def extractreacts():
     endIdxG = 0
@@ -50,16 +72,10 @@ def extractreacts():
     l = 0
     react = ""
     reactdict = {}
-    dbnfileGenes = open(path + "cellfreereacts.react", "w")
-    with open(path + "genes.txt", "r") as g:
-        g.next()
-        for e in g:
-            if "," in e:
-                data = e.strip().split(',')
-                name, Gstart, Gend, Tstart, Tend, idx = data[0:6]
-                assert (int(Gend) - int(Gstart)) == (int(Tend) - int(Tstart))
-                l = extractLSeq(idx)
-                endIdxG, i, reactdict = computereact(idx, Tstart, Tend, l, dbnfileGenes, endIdxG, i, reactdict)
+    filew = openfile("cellfreereacts.react", "w")
+    filer = openfile("genes.txt", "r")
+    extractfilerInfo(filer, filew, endIdxG, i, reactdict)# extract information from read file
+                 
                     
 def extractLSeq(idx):
     with open(path + "cellfree.dbn", "r") as C:
@@ -72,49 +88,43 @@ def extractLSeq(idx):
                     l =  len(seq)
     return l  
 
-def computereact(idx, Tstart, Tend, l, dbnfileGenes, endIdxG, i, reactdict):
-    with open(path + "cellfree.react", "r") as D:
+def computereact(idx, Tstart, Tend, l, filew, endIdxG, i, reactdict):
+    cutoff = 20
+    filer = openfile("cellfree.react", "r")
+    with filer as D:
         for d in D:
             if ">" in d:
-                namesidx = d.strip().split('_')
-                indx = namesidx[len(namesidx)-1]
-                if indx == idx:
+                namesidx = d.strip().split('_')# contains name and index
+                if namesidx[len(namesidx)-1] == idx:
                     i = i + 1
                     for d in D:
-                        indxreact = d.strip().split(' ')
                         if ">" not in d:
-                            index, react = indxreact[0:2]
-                            reactdict[int(index)] = react
+                            index, react = d.strip().split(' ')[0:2] # contains index and reactivity
+                            reactdict[int(index)] = react# contains reactivity
                         else: break
-                    reactdict = collections.OrderedDict(sorted(reactdict.items()))
-                    if endIdxG == 0:
-                        startIdxT = endIdxG
-                    else: startIdxT = endIdxG + 1
-                    startIdxG = int(Tstart)
-                    endIdxG = int(Tend)
-                    endIdxT = startIdxG
-                    nameT = "T_" + idx + "_" + str(i)
+                    reactdict = collections.OrderedDict(sorted(reactdict.items()))# sort reactdict
                     text1= ""
-                    if startIdxT == 0:
-                        endT = endIdxT - 2
-                    else: endT = endIdxT - 1
-
-                    for k in range (startIdxT - 1, endT):
-                        if startIdxT == 0: 
-                            text1 = text1 + str(k - startIdxT + 2) + " " + reactdict.values()[k + 1] + "\n"
-                        else: text1 = text1 + str(k - startIdxT + 2) + " " + reactdict.values()[k] + "\n"
-                    text =">%s\n%s\n" % (nameT, text1)
-                    dbnfileGenes.write(text)
-                    if i == len(namesidx) - 1: 
+                    if endIdxG == 0: # Index of the first transcript
+                        for k in range (endIdxG - 1, int(Tstart) - 2):
+                            text1 = text1 + str(k - endIdxG + 2) + " " + reactdict.values()[k + 1] + "\n"
+                    else: # Index of the rest transcripts
+                        for k in range (endIdxG, int(Tstart) - 1):
+                            text1 = text1 + str(k - endIdxG + 1) + " " + reactdict.values()[k] + "\n" 
+                    if int(Tstart) - endIdxG - 1 >= cutoff:
+                        filew.write(">%s\n%s\n" % ("T_" + idx + "_" + str(i), text1)) # write the transcripts    
+                    endIdxG = int(Tend)
+                    if i == len(namesidx) - 1:  # write the last transcript
                         text1 = ""
                         for k in range (endIdxG, l - 1):
                             text1 = text1 + str(k - endIdxG + 1) + " " + reactdict.values()[k] + "\n"
-                        text = ">%s\n%s\n" % ("T_" + idx + "_" + str(i+1), text1)
-                        dbnfileGenes.write(text)
+                        if l - endIdxG - 1 >= cutoff:
+                            filew.write(">%s\n%s\n" % ("T_" + idx + "_" + str(i+1), text1))
                         endIdxG = 0
                         i = 0
                     break
 
     return (endIdxG, i, reactdict)
+
+    
 
 
