da33d1771907e0ef47887180d1e142c458e61502
S Mautner <mautner@cs.uni-freiburg.de>
2018-07-28 11:49:38 +0200

    finished rm genes

--- data/weeks194_orig/remove_genes.py
+++ data/weeks194_orig/remove_genes.py
@@ -21,43 +21,52 @@ def read_genes():
             name, Gstart, Gend, Tstart, Tend, idx =  e.strip().split(',')
             res[int(idx)].append((int(Tstart),int(Tend))) 
     return res
-            
+
+def read_dbn(fname):
+    '''return sorted dbn ;sort because we will zip with the gene keys'''
+    dbn = rio.read_dbn(fname)
+    dbn.sort( key=lambda x:int(x[0].split("_")[-1]))
+    return dbn
 
 
 ##############
 # phase 2 mixing data
 ##############
 
-def removegenes(gen, dbn, rea, lencutoff=20):
+def removegenes(gen, dbn, rea, lencutoff=10):
     ''' returns {seqname:react} and [seqname, seq]'''
     res_react = {}
     res_dbn = []
     k=gen.keys()
     k.sort()
     for kk, (seqname,seq,_) in zip (k,dbn): # for all transcripts
-        genelocations = gen[kk] + [(0,0)] # fake gene for edge case
+        genelocations = [(0,0)]+gen[kk] # fake gene for edge case
         genelocations.reverse()
         react = rea[seqname]
-        
         for i, (start, stop )in enumerate(genelocations): # for all genes
             myreact = react[stop:]
-            react = react[:start]
+            react = react[:start-1]
             myseq = seq[stop:]
-            seq = seq[:start]
-            if myseq > lencutoff:
-                res_react[seqname+str(i)] = myreact
-                res_dbn.append([seqname+str(i),myseq])
+            seq = seq[:start-1]
+            if len(myseq) > lencutoff:
+                sequencename = "%s_%d" % (seqname, i)
+                res_react[sequencename] = myreact
+                res_dbn.append([sequencename,myseq,'.'*len(myseq)])
 
     return res_react, res_dbn
 
 ##############################
-# now there should be a way to dump le data, but that seems trivial, also there might be off-by-1-errors here
+# phase 3 dump data 
 ###########################
 
 cdata = 'incell'            
 
 gen = read_genes()
-dbn = rio.read_dbn(cdata+".dbn")
+dbn = read_dbn(cdata+".dbn")
 rea = rio.read_react(cdata+".react")
 react_dict, fasta_list = removegenes(gen,dbn,rea)
+rio.dump_shape(react_dict, cdata+"_nogenes.react")
+rio.dump_dbn(fasta_list, cdata+"_nogenes.dbn")
+
+
 
