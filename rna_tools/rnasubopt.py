1a18b0aa513d116c078986439273a1f41b104bab
S Mautner <mautner@cs.uni-freiburg.de>
2018-07-30 23:01:43 +0100

    hayaku!

--- rna_tools/rnasubopt.py
+++ rna_tools/rnasubopt.py
@@ -9,20 +9,22 @@ def rnasubopt(sequence, return_energy=False):
         #print "echo " + "%s" % sequence + " | RNAsubopt -p 60 --maxBPspan=150"
 
         d={}
-        if len(sequence) > 300:
-            if os.path.isfile(".rnasubopt_cache"):
-                d = rna_tools.loadfile(".rnasubopt_cache")
-                if sequence in d:
-                    return d[sequence] if return_energy else d[sequence][0]
-            else:
-                rna_tools.dumpfile({}, '.rnasubopt_cache')
+        if False: # deactivated cache...
+            if len(sequence) > 300:
+                if os.path.isfile(".rnasubopt_cache"):
+                    d = rna_tools.loadfile(".rnasubopt_cache")
+                    if sequence in d:
+                        return d[sequence] if return_energy else d[sequence][0]
+                else:
+                    rna_tools.dumpfile({}, '.rnasubopt_cache')
+
         retcode, err, out = shexec("echo " + "%s" % sequence + " | RNAsubopt -p 60 --maxBPspan=150")
         energy = re.findall(r"[-+]?[0-9]*\.?[0-9]+", out)
         shape = [a.strip() for a in re.findall(r'\n[.()]+', out)]
 
         if len(sequence) > 300:
             d[sequence]= (shape, energy)
-            rna_tools.dumpfile(d,".rnasubopt_cache")
+            #rna_tools.dumpfile(d,".rnasubopt_cache")
 
         if return_energy:
             return shape, energy
